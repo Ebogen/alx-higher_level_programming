c26b2d7664e088f83f2789508bdd9956d2e51b7e
Ebogen <ebogensamuel@gmail.com>
2022-07-19 21:22:51 -0700

    task 4 and 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" Here we define a square by its size"""
+
+
+""" We defines a square by its size """
 
 
 class Square:
-    """ initialize the square object"""
+    """ Here we initialize the square object """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -13,20 +16,34 @@ class Square:
             self.__size = size
 
     def area(self):
-        """ return the square area of the object """
+        """ returns the square are of the object """
+
         return (self.__size ** 2)
 
     @property
     def size(self):
-        """ return the size value"""
+        """ returns the size value """
+
         return self.__size
 
     @size.setter
     def size(self, value):
         """ set the size value of the square object """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def my_print(self):
+        """ prints a # square according to the size value
+        """
+        if not self.__size:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
