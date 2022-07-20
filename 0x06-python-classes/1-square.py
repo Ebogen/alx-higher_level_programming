32edcd3403fc128022b25f7fc6fce30340b8fc63
Ebogen <ebogensamuel@gmail.com>
2022-07-19 20:16:37 -0700

    a class Square that defines a square whose size is private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+""" Class : Square """
+
+
+class Square:
+
+    """ Here we define a class known as Square with a size """
+    def __init__(self, size):
+        """ Here we initialize to store the size of the square """
+        self.__size = size
