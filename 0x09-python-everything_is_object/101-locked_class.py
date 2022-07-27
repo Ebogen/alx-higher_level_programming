084d90b1e50e8db350df9201feec4ae794949813
Ebogen <ebogensamuel@gmail.com>
2022-07-26 17:59:50 -0700

    task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-A module that contains a class that avoids
-dynmaically created attributes
+    101-locked_class: class LockedClass
 """
 
 
 class LockedClass:
+    """
+        A class that can only have one attribute first_name.
+        Attribute:
+             first_name (str): name
+    """
     __slots__ = ['first_name']
