3b6dc8adef9709e71363ecdae5889e6057d9f8fb
Ebogen <ebogensamuel@gmail.com>
2022-08-02 14:08:43 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+An Empty Class
+"""
+
+
+class BaseGeometry:
+    """
+    A method that raises an exception message
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
