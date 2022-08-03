4aa5357e2e111736c06996be3c93a0ece765078e
Ebogen <ebogensamuel@gmail.com>
2022-08-02 20:32:51 -0700

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """
-A function that returns the pascal triangle
-Argument:
-n: number of lines
-
-Return:
-Empty list: if n <= 0 Otherwise pascal triangle
+12-pascal_triangl e: pascal_triangle()
 """
 
 
 def pascal_triangle(n):
+    """
+        returns a list of lists of integers
+        Args:
+            n (int): number of lists and digits
+            l (int): represents lists
+        Returns: list
+    """
+    t_row = [1]
+    temp_l = [0]
+    pTri = []
 
     if n <= 0:
-        return ([])
+        return pTri
 
-    pascal_t = [[1]]
-    for i in range(1, n):
-        row = [1]
-        prev = pascal_t[i - 1]
-        for j in range(len(prev)):
-            new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
-            row.append(new)
-
-        pascal_t.append(row)
-
-    return pascal_t
+    for i in range(n):
+        pTri.append(t_row)
+        t_row = [l+r for l, r in zip(t_row + temp_l, temp_l + t_row)]
+    return pTri
