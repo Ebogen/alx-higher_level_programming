33827d1c3d3a41dee0e901eef07559e86dcdf661
Ebogen <ebogensamuel@gmail.com>
2022-08-07 08:04:41 -0700

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module containing class Base
+"""
+
+import json
+import csv
+import os.path
+
+
+class Base:
+    """class base for the entire project. """
+    __nb_objects = 0
+
+    def __init__(self, id = None):
+        """ Next we initialize the class attributes """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objexts += 1
+            self.id = Base.__nb_objects
+            
