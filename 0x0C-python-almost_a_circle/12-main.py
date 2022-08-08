edff1b24456b0c5f8505788d6b719bc23d0b8147
Ebogen <ebogensamuel@gmail.com>
2022-08-08 16:40:19 -0700

    more task

--- 0x0C-python-almost_a_circle/12-main.py
+++ 0x0C-python-almost_a_circle/12-main.py
@@ -1,3 +1,17 @@
-git add .
-git commit -m '0x0C-python-almost_a_circle'
-git push
+#!/usr/bin/python3
+""" 12-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 2, 1, 9)
+    print(r1)
+    r1_dictionary = r1.to_dictionary()
+    print(r1_dictionary)
+    print(type(r1_dictionary))
+
+    r2 = Rectangle(1, 1)
+    print(r2)
+    r2.update(**r1_dictionary)
+    print(r2)
+    print(r1 == r2)
